922a6554beceb6ec06bf14af0b026991bf55a78f
kotbi abderrahmane <kotbymo@gmail.com>
2021-12-28 18:58:49 +0100

    add social graph service

---
+++ social-graph-service/src/main/java/com/ensias/socialgraph/repository/search/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Data Elasticsearch repositories.
+ */
+package com.ensias.socialgraph.repository.search;
---
+++ social-graph-service/src/test/java/com/ensias/socialgraph/IntegrationTest.java
@@ -0,0 +1,17 @@
+package com.ensias.socialgraph;
+
+import com.ensias.socialgraph.SocialgraphApp;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = SocialgraphApp.class)
+public @interface IntegrationTest {
+}
